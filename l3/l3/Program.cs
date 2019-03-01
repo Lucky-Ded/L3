c124956d0f56a13bd28d2ad9f452083be2988a6d
POPEYE <123>
2019-03-01 13:46:27 +0700

    multi

--- l3/l3/Program.cs
+++ l3/l3/Program.cs
@@ -19,6 +19,8 @@ namespace l3
             //получение символа нажатой клавиши (если возможно)
             char ch = key.KeyChar;
 
+            if (ch == '*') Console.WriteLine("{0}", a * b);
+            Console.ReadKey();
         }
     }
 }
